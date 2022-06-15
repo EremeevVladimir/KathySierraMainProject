786772e0a2858435e71c41a9f4283ab2388bf35b
Vladimir <vladstark2000@gmail.com>
2022-06-15 21:11:48 +0300

    half BeatBox class

--- src/BeatBox/BeatBox.java
+++ src/BeatBox/BeatBox.java
@@ -1,9 +1,41 @@
 package BeatBox;
 
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class BeatBox {
     JPanel mainPanel;
-    //ArrayList<JCheckBox> checkBoxList
+    ArrayList<JCheckBox> checkBoxList;
+    Sequencer sequencer;
+    Track track;
+    JFrame theFrame;
+
+    String [] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Snare", "Crash Cymbal",
+            "Hand Clap", "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap",
+            "Low-mid Tom", "High Agogo", "Open Hi Conga"};
+    int [] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+
+    public static void main (String [] args) {
+        new BeatBox2().buildGUI();
+    }
+
+    public void buildGUI() {
+        theFrame = new JFrame("Cyber BeatBox");
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        checkBoxList = new ArrayList<JCheckBox>();
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+
+        JButton start = new JButton("Start");
+        start.addActionListener(new MyStartListener());
+        buttonBox.add(start);
+        
+
+    }
 }
