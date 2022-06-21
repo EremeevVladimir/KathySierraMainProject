da7f3fa1236893a45618908397d900964c4e74a6
Vladimir <vladstark2000@gmail.com>
2022-06-21 18:48:58 +0300

    21/06/22 minor changes

--- src/SmallExamples/ReadAFile.java
+++ src/SmallExamples/ReadAFile.java
@@ -9,20 +9,20 @@ public class ReadAFile {
     public static void main(String[] args) {
 
         try {
-            File myFile = new File("MyText.txt");
-            FileReader fileReader = new FileReader(myFile);
+            File myFile = new File("MyText.txt"); //создаем объект класса File представляющий файл
+            FileReader fileReader = new FileReader(myFile); //подключаем его к потоку для соединения FileReader
 
-            BufferedReader reader = new BufferedReader(fileReader);
+            BufferedReader reader = new BufferedReader(fileReader); //Создаем буфер и подключаем его к потоку для соединения
 
             String line = null;
 
-            while ((line=reader.readLine()) != null) {
+            while ((line=reader.readLine()) != null) { //чтение происходит в цикле while
                 System.out.println(line);
             }
 
 
         } catch (Exception ex) {
-
+            ex.printStackTrace();
 
         }
     }
